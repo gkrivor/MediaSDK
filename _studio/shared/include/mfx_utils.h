4aa11611c8858081f37f043d6c3c3be34f6af973
Natalia Domnina <natalia.domnina@intel.com>
2018-11-15 17:36:08 +0300

    [VP9] Fixed y410 processing
    Working with y410 and system memory it is necessary to check
    Data.Y410->Y instead of Data.Y

--- _studio/shared/include/mfx_utils.h
+++ _studio/shared/include/mfx_utils.h
@@ -99,9 +99,9 @@ mfxU64 GetMfxTimeStamp(mfxF64 ts)
 }
 
 inline
-bool LumaIsNull(mfxFrameSurface1 * surf)
+bool LumaIsNull(const mfxFrameSurface1 * surf)
 {
-    return !surf->Data.Y && !surf->Data.Y410;
+    return !surf->Data.Y410;
 }
 
 #define MFX_ZERO_MEM(_X)    memset(&_X, 0, sizeof(_X))
